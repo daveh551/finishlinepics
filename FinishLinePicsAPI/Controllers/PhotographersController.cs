5f4cef488a84bfe3e6a08b300a3c15d1bf849237
Dave Hanna <dave@infotouchsys.com>
2015-07-18 16:48:53 -0500

    Scaffold Photos, Events, and Photographers controllers

---
+++ FinishLinePicsAPI/Controllers/PhotographersController.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using FinishLinePics.Models;
+using FinishLinePics.Models.Entities;
+
+namespace FinishLinePicsAPI.Controllers
+{
+    public class PhotographersController : ApiController
+    {
+        private photodataContext db = new photodataContext();
+
+        // GET: api/Photographers
+        public IQueryable<Photographer> GetPhotographers()
+        {
+            return db.Photographers;
+        }
+
+        // GET: api/Photographers/5
+        [ResponseType(typeof(Photographer))]
+        public async Task<IHttpActionResult> GetPhotographer(int id)
+        {
+            Photographer photographer = await db.Photographers.FindAsync(id);
+            if (photographer == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(photographer);
+        }
+
+        // PUT: api/Photographers/5
+        [ResponseType(typeof(void))]
+        public async Task<IHttpActionResult> PutPhotographer(int id, Photographer photographer)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != photographer.Id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(photographer).State = EntityState.Modified;
+
+            try
+            {
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PhotographerExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Photographers
+        [ResponseType(typeof(Photographer))]
+        public async Task<IHttpActionResult> PostPhotographer(Photographer photographer)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Photographers.Add(photographer);
+            await db.SaveChangesAsync();
+
+            return CreatedAtRoute("DefaultApi", new { id = photographer.Id }, photographer);
+        }
+
+        // DELETE: api/Photographers/5
+        [ResponseType(typeof(Photographer))]
+        public async Task<IHttpActionResult> DeletePhotographer(int id)
+        {
+            Photographer photographer = await db.Photographers.FindAsync(id);
+            if (photographer == null)
+            {
+                return NotFound();
+            }
+
+            db.Photographers.Remove(photographer);
+            await db.SaveChangesAsync();
+
+            return Ok(photographer);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool PhotographerExists(int id)
+        {
+            return db.Photographers.Count(e => e.Id == id) > 0;
+        }
+    }
+}
---
+++ FinishLinePicsAPI/Controllers/PhotosController.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using FinishLinePics.Models;
+using FinishLinePics.Models.Entities;
+
+namespace FinishLinePicsAPI.Controllers
+{
+    public class PhotosController : ApiController
+    {
+        private photodataContext db = new photodataContext();
+
+        // GET: api/Photos
+        public IQueryable<Photo> GetPhotos()
+        {
+            return db.Photos;
+        }
+
+        // GET: api/Photos/5
+        [ResponseType(typeof(Photo))]
+        public async Task<IHttpActionResult> GetPhoto(Guid id)
+        {
+            Photo photo = await db.Photos.FindAsync(id);
+            if (photo == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(photo);
+        }
+
+        // PUT: api/Photos/5
+        [ResponseType(typeof(void))]
+        public async Task<IHttpActionResult> PutPhoto(Guid id, Photo photo)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != photo.Id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(photo).State = EntityState.Modified;
+
+            try
+            {
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PhotoExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Photos
+        [ResponseType(typeof(Photo))]
+        public async Task<IHttpActionResult> PostPhoto(Photo photo)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Photos.Add(photo);
+
+            try
+            {
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (PhotoExists(photo.Id))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtRoute("DefaultApi", new { id = photo.Id }, photo);
+        }
+
+        // DELETE: api/Photos/5
+        [ResponseType(typeof(Photo))]
+        public async Task<IHttpActionResult> DeletePhoto(Guid id)
+        {
+            Photo photo = await db.Photos.FindAsync(id);
+            if (photo == null)
+            {
+                return NotFound();
+            }
+
+            db.Photos.Remove(photo);
+            await db.SaveChangesAsync();
+
+            return Ok(photo);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool PhotoExists(Guid id)
+        {
+            return db.Photos.Count(e => e.Id == id) > 0;
+        }
+    }
+}
